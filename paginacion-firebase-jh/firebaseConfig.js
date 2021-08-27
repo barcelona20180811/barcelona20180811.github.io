62b5158877cefd05b8bd3b8f4ca046144f8106aa
José Herrero <barcelona20180811@gmail.com>
2021-08-27 10:55:05 +0200

    agrego proyecto paginacion-firebase-jh

---
+++ paginacion-firebase-jh/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD2LCm0mnvsDXRYTC6f6E1y88ePYXinAvI",
+  authDomain: "paginacion-firebase-a2e9d.firebaseapp.com",
+  projectId: "paginacion-firebase-a2e9d",
+  storageBucket: "paginacion-firebase-a2e9d.appspot.com",
+  messagingSenderId: "204916077117",
+  appId: "1:204916077117:web:439eb7367be14e4e7b7054"
+};
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
