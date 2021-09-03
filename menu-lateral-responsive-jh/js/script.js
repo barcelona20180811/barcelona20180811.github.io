fd3661b8a41ee353c6c1937f27f7d11a9fdde2c7
José Herrero <barcelona20180811@gmail.com>
2021-09-03 09:29:44 +0200

    agrego proyecto menu-lateral-responsive-jh

---
+++ menu-lateral-responsive-jh/js/script.js
@@ -0,0 +1,35 @@
+//Ejecutar función en el evento click
+document.getElementById("btn_open").addEventListener("click", open_close_menu);
+
+//Declaramos variables
+let menu_side = document.getElementById("menu_side");
+let btn_open = document.getElementById("btn_open");
+let body = document.getElementById("body");
+
+//Evento para mostrar y ocultar menú
+function open_close_menu() {
+  body.classList.toggle("body_move");
+  menu_side.classList.toggle("menu__side_move");
+}
+
+//Si el ancho de la página es menor a 760px,
+//ocultará el menú al recargar la página
+
+if (window.innerWidth < 760) {
+  body.classList.add("body_move");
+  menu_side.classList.add("menu__side_move");
+}
+
+//Haciendo el menú responsive(adaptable)
+
+window.addEventListener("resize", function () {
+  if (window.innerWidth > 760) {
+    body.classList.remove("body_move");
+    menu_side.classList.remove("menu__side_move");
+  }
+
+  if (window.innerWidth < 760) {
+    body.classList.add("body_move");
+    menu_side.classList.add("menu__side_move");
+  }
+});
